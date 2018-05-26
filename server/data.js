2ac1d4dc7590505b1743c13cc04e3db5515cd6b8
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2018-05-26 09:27:59 -0600

    Initial commit

---
+++ server/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        firstName: 'Lewis',
+        lastName: 'de Gada',
+        gpa: 3.1,
+        birthDate: new Date(2003, 3, 17),
+    },
+    {
+        firstName: 'Jack',
+        lastName: 'Yamada',
+        gpa: 3.8,
+        birthDate: new Date(2003, 7, 16),
+    },
+    {
+        firstName: 'Mary',
+        lastName: 'Willard',
+        gpa: 4.0,
+        birthDate: new Date(2002, 10, 3),
+    },
+    {
+        firstName: 'Edna',
+        lastName: 'Varion',
+        gpa: 2.9,
+        birthDate: new Date(2002, 9, 26),
+    },
+];
